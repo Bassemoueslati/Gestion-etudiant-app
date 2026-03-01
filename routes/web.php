1a0e2d0a7d90821dc370e448d7cae4942fd0cc52
Bassem Oueslati <bassemoueslati707@gmail.com>
2026-03-01 17:28:25 +0000

    Update web routes to return JSON responses

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,30 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EtudiantContoller;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\JsonResponse;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application.
+|
+*/
+
+// Simple API-like responses for web
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'app' => 'Gestion Etudiant',
+        'version' => '1.0.0',
+        'message' => 'Welcome to Gestion Etudiant API'
+    ]);
 });
 
 Route::group(['middleware' => ['auth']], function (){
-    Route::get('/etudiant',[EtudiantContoller::class,"index"])->name('etudiant');
-    Route::get('/create',[EtudiantContoller::class,'create'])->name('etudiant.create');
-    Route::post('/create',[EtudiantContoller::class,'store'])->name('etudiant.ajouter');
-
-    Route::put('/etudiant/{etudiant}',[EtudiantContoller::class,"update"])->name('etudiant.update');
-    Route::get('/etudiant/{etudiant}',[EtudiantContoller::class,"edit"])->name('etudiant.edit');
-
-    Route::get('/show/{etudiant}',[EtudiantContoller::class,"show"])->name('etudiant.show');
-    Route::delete('/delete/{etudiant}',[EtudiantContoller::class,"delete"])->name('etudiant.delete');
+    Route::get('/etudiant', [EtudiantContoller::class, "index"])->name('etudiant');
+    Route::post('/etudiant', [EtudiantContoller::class, "store"])->name('etudiant.store');
+    Route::put('/etudiant/{etudiant}', [EtudiantContoller::class, "update"])->name('etudiant.update');
+    Route::get('/etudiant/{etudiant}', [EtudiantContoller::class, "show"])->name('etudiant.show');
+    Route::delete('/etudiant/{etudiant}', [EtudiantContoller::class, "delete"])->name('etudiant.delete');
 });
